d79e8c2f0089c9e758f45b0bb8d582cb20c6cd41
Kaushal Behere <kaushalpbehere@gmail.com>
2021-05-07 21:05:00 +0100

    Ajax call

--- public/js/script.js
+++ public/js/script.js
@@ -8,9 +8,23 @@ weatherForm.addEventListener('submit', (e) => {
     //Location value is with us now. 
     /* If there is an error make sure you add link to this script file
      the the bottom of the page instead of adding it into the header. */
-    fetch('/weatherData?address=' + location)
-        .then((response) => {
+    // fetch('/weatherData?address=' + location)
+    //     .then((response) => {
+    //         console.log(response.forecast)
+    //         console.log(response.location)
+    //     })
+
+
+    $.ajax({
+        type: 'GET',
+        url: '/weatherData?address=' + location,
+        success: function (response) {
+            console.log(response)
             console.log(response.forecast)
             console.log(response.location)
-        })
+        },
+        error: function (xhr, status, err) {
+            console.log(xhr.responseText)
+        }
+    })
 })
